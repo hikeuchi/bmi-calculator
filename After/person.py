aebebaac57b1224afa857f6324a1d8485f81c47f
hikeuchi <hikeuchi@morisawa.co.jp>
2021-07-04 23:35:37 +0900

    create after code

--- After/person.py
+++ After/person.py
@@ -1,32 +1,48 @@
 from dataclasses import dataclass
 
 
+def _calculate_bmi(weight, height) -> float:
+    return round(weight / (height ** 2))
+
+
+def _get_bmi_result(bmi: float) -> str:
+    if bmi < 18.5:
+        result = "痩せ型"
+    elif (bmi >= 18.5) and (bmi < 25):
+        result = "標準体型"
+    elif (bmi >= 25) and (bmi < 30):
+        result = "肥満(軽)"
+    else:
+        result = "肥満(重)"
+    
+    return result
+
+
 @dataclass
 class Person():
-    weight: int  # 体重(kg)
+    weight: float  # 体重(kg)
     height: float  # 身長（m）
-    
+
+    @classmethod
+    def create(cls, weight: float, height: float) -> 'Person':
+        height_meter = float(height) / 100
+        return Person(
+            weight=weight,
+            height=height_meter
+        )
+
     @property
     def bmi(self) -> float:
-        return round(self.weight / (self.height ** 2))
+        return _calculate_bmi(weight=self.weight, height=self.height)
 
-    def print_bmi(self) -> None:
-        bmi = round(self.weight / (self.height ** 2))
-        if bmi < 18.5:
-            result = "痩せ型"
-        elif (bmi >= 18.5) and (bmi < 25):
-            result = "標準体型"
-        elif (bmi >= 25) and (bmi < 30):
-            result = "肥満(軽)"
-        else:
-            result = "肥満(重)"
-        print("BMI: " + str(bmi))
-        print("判定: " + result)
+    def print_bmi_result(self) -> None:
+        print("BMI: " + str(self.bmi))
+        print("判定: " + _get_bmi_result(self.bmi))
 
 
 def main():
-    person = Person(weight=50, height=165)
-    person.print_bmi()    
+    person = Person.create(weight=50, height=160)
+    person.print_bmi_result()
 
 
 if __name__ == "__main__":
