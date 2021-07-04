e6f2f7233d175e61b27e91bf1a39166a7bc8695e
hikeuchi <hikeuchi@morisawa.co.jp>
2021-07-04 23:43:12 +0900

    refactoring of after code

--- After/person.py
+++ After/person.py
@@ -5,6 +5,13 @@ def _calculate_bmi(weight, height) -> float:
     return round(weight / (height ** 2))
 
 
+def _calculate_recommended_weight(height: float) -> float:
+    recommended_weight = (height ** 2) * 22
+    recommended_weight = round(recommended_weight, 1)  # 小数点以下1位で丸める
+    
+    return recommended_weight
+
+
 def _get_bmi_result(bmi: float) -> str:
     if bmi < 18.5:
         result = "痩せ型"
@@ -24,11 +31,11 @@ class Person():
     height: float  # 身長（m）
 
     @classmethod
-    def create(cls, weight: float, height: float) -> 'Person':
-        height_meter = float(height) / 100
+    def create(cls, weight: float, height_cm: float) -> 'Person':
+        height_m = float(height_cm) / 100
         return Person(
             weight=weight,
-            height=height_meter
+            height=height_m
         )
 
     @property
@@ -36,12 +43,13 @@ class Person():
         return _calculate_bmi(weight=self.weight, height=self.height)
 
     def print_bmi_result(self) -> None:
-        print("BMI: " + str(self.bmi))
-        print("判定: " + _get_bmi_result(self.bmi))
+        print(f'BMI: {self.bmi}')
+        print(f'判定: {_get_bmi_result(self.bmi)}')
+        print(f'適正体重: {_calculate_recommended_weight(height=self.height)}')
 
 
 def main():
-    person = Person.create(weight=50, height=160)
+    person = Person.create(weight=50, height_cm=160)
     person.print_bmi_result()
 
 
